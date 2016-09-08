4364e5af5757ff2b3499a10dc2c32fd56687757d
Ross Miller <rossnmiller@gmail.com>
2016-09-08 11:31:10 -0700

    Initial commit. Uses redux-slingshot as a skeleton

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={FuelSavingsPage}/>
+    <Route path="*" component={FuelSavingsPage}/>
+  </Route>
+);
